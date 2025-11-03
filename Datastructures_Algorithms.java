fa6a3f822af8e0249e0583174f2717df8ba99a34
Syed40A <72885427+Syed40H@users.noreply.github.com>
2025-11-02 20:44:32 -0500

    Learning2

---
+++ Datastructures_Algorithms.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class Datastructures_Algorithms {
+    public static void main(String[] args) {
+ /* 
+        Stack<String> stack = new Stack<String>();
+
+        // ****************************************************************
+        // stack = LIFO data structure. Last-In First-Out
+        //          stores objectrs into a sort of "vertical tower"
+        //          push() to add to the top
+        //          pop() to remove from the top
+        // ****************************************************************
+
+        //System.out.println(stack.empty());
+        stack.push("Minecraft");
+        stack.push("GTA6")\
+        stack.push("FC26");
+        stack.push("BlackOps");
+        stack.push("Valorant");
+
+        //System.out.println(stack.empty());
+        //stack.pop();
+        //String myFavGame = stack.pop();
+        //System.out.println(myFavGame);
+        System.out.println(stack.peek());
+        System.out.println(stack.search("GTA6"));
+        System.out.println(stack);
+*/
+        
+    }
+}
